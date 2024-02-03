efb6e889b05af94f1d75062a5fea3e07dc46e6cc
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-02-03 10:17:24 +0530

    axios

---
+++ front/src/Api.tsx
@@ -0,0 +1,33 @@
+// Example React component to integrate with Weavy's APIs
+
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
+const WeavyIntegration = () => {
+  // State to hold users data
+  const [users, setUsers] = useState([]);
+
+  // Fetch users data from Weavy API on component mount
+  useEffect(() => {
+    axios.get('"GET /api/users/{id}')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Weavy Users</h1>
+      {/*<ul>*/}
+      {/*  {users.map(user => (*/}
+      {/*    <li key={user.id}>{user.name}</li>*/}
+      {/*  ))}*/}
+      {/*</ul>*/}
+    </div>
+  );
+};
+
+export default WeavyIntegration;
