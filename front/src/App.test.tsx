2f0da8e4c58a2da235d8791c1deb6cc63e974d1b
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2024-02-03 10:12:49 +0530

    creating components

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// App.tsx
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import React from 'react';
+import Button from './components/Button';
+
+const App: React.FC = () => {
+  const handleClick = () => {
+    alert('Button clicked!');
+  };
+
+  return (
+    <div>
+      <h1>My React App</h1>
+      <Button onClick={handleClick} text="Click me" />
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Button.tsx
@@ -0,0 +1,18 @@
+// Button.tsx
+
+import React from 'react';
+
+interface ButtonProps {
+  onClick: () => void;
+  text: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  );
+}
+
+export default Button;
